7673401f47172e03bc0c7b6affc8310da33cbdbc
Zero Suxx <tomi@mohos.name>
2017-11-06 19:58:32 +0000

    [Added] pagination

--- script.js
+++ script.js
@@ -2,29 +2,37 @@
 angular.module('myapp',['myapp.controller']);
 
 angular.module('myapp.controller',['myapp.service'])
-  .controller('testController',function($scope,testService){
-
-    $scope.posts={};
-    function GetAllPosts() {
-      var getPostsData = testService.getPosts();
-
-      getPostsData.then(function (post) {
-         $scope.posts = post.data;
-
-      }, function () {
-         alert('Error in getting post records');
-      });
+  .controller('testController', function($scope, httpService) {
+    $scope.page = 1;
+    $scope.posts = {};
+    function getComments(page) {
+		httpService.getComments(page).then(function(post) {
+			$scope.posts = post.data;
+		}, function () {
+			alert('Error in getting comments records');
+		});
     }
           
-    GetAllPosts();
+    getComments($scope.page);
+    
+    $scope.loadNextPage = function() {
+        $scope.page++;
+        getComments($scope.page);
+    };
+	
+	$scope.loadPrevPage = function() {
+		if($scope.page > 1) {
+			$scope.page--;
+			getComments($scope.page);
+		}
+    };
 });
 
 angular.module('myapp.service',[])
-  .service('testService', function ($http) {
+  .service('httpService', function ($http) {
 
-    //get All NewsLetter
-    this.getPosts = function () {
-       return $http.get('https://jsonplaceholder.typicode.com/posts');
+    //get All Comments
+    this.getComments = function (page) {
+       return $http.get('https://jsonplaceholder.typicode.com/posts/'+page+'/comments');
     };
  });
-
